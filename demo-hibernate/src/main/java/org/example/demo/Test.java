e8827ac945b73df0832b59b154ef70481a01b093
zhi4chi2 <yu_huanchao@163.com>
2017-06-14 17:09:52 +0800

    add hibernate jpa persistence.xml

--- demo-hibernate/src/main/java/org/example/demo/Test.java
+++ demo-hibernate/src/main/java/org/example/demo/Test.java
@@ -1,6 +1,11 @@
 package org.example.demo;
 
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Test {
     public static void main(String[] args) {
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("test");
+        factory.close();
     }
 }
