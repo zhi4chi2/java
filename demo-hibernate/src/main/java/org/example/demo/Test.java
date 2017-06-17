5e99e35775a4399affe83eae36f18c28e12e8205
zhi4chi2 <yu_huanchao@163.com>
2017-06-17 20:22:48 +0800

    move Person to Test

--- demo-hibernate/src/main/java/org/example/demo/Test.java
+++ demo-hibernate/src/main/java/org/example/demo/Test.java
@@ -1,19 +1,32 @@
 package org.example.demo;
 
+import javax.persistence.Entity;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.Persistence;
 
 public class Test {
     public static void main(String[] args) {
         EntityManagerFactory factory = Persistence.createEntityManagerFactory("test");
         EntityManager em = factory.createEntityManager();
+
         em.getTransaction().begin();
         Person person = new Person();
-        person.setName("测试人");
+        person.name = "测试人";
         em.persist(person);
         em.getTransaction().commit();
+
         em.close();
         factory.close();
+    }
+
+    @Entity(name = "Person")
+    public static class Person {
+        @Id
+        @GeneratedValue
+        Integer id;
+        String name;
     }
 }
